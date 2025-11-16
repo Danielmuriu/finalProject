a5682a5924da8f6466d5eb7a7f79764dd523d611
Danielmuriu <muriudaniel7@gmail.com>
2025-11-16 11:55:36 +0000

    updated

--- my-project/src/components/sections/Products.jsx
+++ my-project/src/components/sections/Products.jsx
@@ -58,7 +58,9 @@ const Products = () => {
         ))}
       </SectionWrapper>
       <div className="max-w-7xl mx-auto text-center mt-12 px-6 lg:px-20 pb-20">
-        <Button variant="outline">View More Items</Button>
+        <a href="https://www.google.com/search?client=firefox-b-d&sca_esv=2b4071618c37111c&q=gifts+paradise+kenya&source=lnms&fbs=AIIjpHzKhFDFGqdV4Dt4FtciRsW5-Ar5zavWI2ZC-nhKeGNRvi16RI88_fFb_kOAUKWxWH6wLTMg6qLR1w14dIDkArQIHyIBAP60FAYNPFolW_hR_UFgWK-958gjOScnHJ2kybZLsWP5uoelqttTYYOpZ-zaxGUkZFr5Zzos0W9Jmmqf5oQ7tTAHWa9CiqOUlCy4I-uyIyNH&sa=X&ved=2ahUKEwj1ze2VwPaQAxUi9wIHHfAIDkwQ0pQJegQICRAB&biw=1516&bih=568&dpr=1#lpg=cid:CgIgAQ%3D%3D,ik:CAoSLEFGMVFpcE1QNFRlU1MxUUsyWUZ4bGE4SEp2R3ptWThMZndmN1VjZDY0bGRF" target="_blank" rel="noopener noreferrer">
+          <Button variant="outline">View More Items</Button>
+        </a>
       </div>
     </div>
   );
